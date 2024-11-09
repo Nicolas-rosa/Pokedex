12331e2fbf76aa2f15e33e1ed89011e95a8c2bbb
Nicolas Rosa <nicolasfrancacastrorosa@gmail.com>
2024-11-09 02:11:57 -0300

    descrição do commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,51 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 function App() {
-  // Define um estado para armazenar os Pokémon, inicialmente como um array vazio
   const [pokemons, setPokemons] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  // Função para buscar informações de um Pokémon pelo ID
   const getPokemons = (id) => {
     return axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then(response => {
-        const pokemon = response.data; // Armazena os dados do Pokémon retornados
-        // Atualiza o estado com o novo Pokémon, mantendo os anteriores
-        setPokemons((prevPokemons) => [...prevPokemons, pokemon]);
-      });
+      .then(response => response.data);
   };
 
-  // Função para buscar informações de 150 Pokémon
   const arrayPokemons = () => {
-    return Promise.all(Array(150).fill().map((_, index) => getPokemons(index + 1)));
+    return Promise.all(Array.from({ length: 150 }, (_, index) => getPokemons(index + 1)));
   };
 
-  // useEffect é um hook que executa um efeito colateral neste caso, buscar os Pokémon quando o componente é montado
   useEffect(() => {
-    arrayPokemons().then(() => {
-      console.log('Todos os Pokémon foram carregados'); // Log quando todos os Pokémon são carregados
+    arrayPokemons().then(pokemonData => {
+      setPokemons(pokemonData);
     });
-  }, []); // O array vazio significa que o efeito será executado apenas uma vez, quando o componente for montado
+  }, []);
 
-  // Renderiza o componente
+  const handleSearch = () => {
+    if (searchTerm.trim() === '') {
+      return pokemons; // Se o termo de pesquisa estiver vazio, retorne todos os Pokémon
+    }
+
+    return pokemons.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
+
+  const filteredPokemons = handleSearch();
+
   return (
     <Container>
       <Card>
         <Title>Pokédex</Title>
+        <SearchContainer>
+          <SearchInput
+            type="text"
+            placeholder="Pesquisar Pokémon..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        
+        </SearchContainer>
         <PokemonList>
-          {pokemons.map((pokemon) => (
+          {filteredPokemons.map((pokemon) => (
             <PokemonCard key={pokemon.id}>
               <CardContent>
                 <PokemonImage src={pokemon.sprites.front_default} alt={pokemon.name} />
@@ -58,65 +71,113 @@ function App() {
 }
 
 
+
+
+// Container principal
 const Container = styled.div`
-  display: flex; // Usa flexbox para alinhar o conteúdo
-  justify-content: center; // Centraliza horizontalmente
-  align-items: center; // Centraliza verticalmente
-  min-height: 100vh; // Define a altura mínima como 100% da altura da viewport
-  background: rgb(76,0,255);
-background: linear-gradient(90deg, rgba(76,0,255,1) 0%, rgba(29,244,253,1) 50%, rgba(96,69,252,1) 100%); //cor do fundo
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: linear-gradient(90deg, rgba(0, 0, 30, 0.8), rgba(0, 0, 60, 0.8)); // Fundo escuro
+  backdrop-filter: blur(10px); // Efeito de desfoque
+  padding: 20px;
+  font-family: 'Press Start 2P', cursive; // Fonte inspirada em Pokémon
 `;
 
+// Card principal
 const Card = styled.div`
-  background-color: red; // Cor de fundo do card
-  border-radius: 10px; // Bordas arredondadas
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1); // Sombra para dar profundidade
-  padding: 20px; // Espaçamento interno
-  width: 25%; // Largura do card
-`;
-
-
-const Title = styled.h1`
-  text-align: center; // Centraliza o texto
-  color: white; // Cor do texto
-`;
-
-const PokemonList = styled.ul`
-  list-style-type: none; // Remove os marcadores da lista
-  padding: 0; // Remove o preenchimento padrão
-`;
-
-
-const PokemonCard = styled.li`
-  border: 1px solid #ddd; // Borda cinza clara
-  border-radius: 10px; // Bordas arredondadas
-  margin: 10px 0; // Margem vertical
-  padding: 10px; // Espaçamento interno
-  background-color: #f9f9f9; // Cor de fundo do card
-  transition: transform 0.2s; // Transição suave para transformações
+  background: rgba(30, 30, 30, 0.8); // Fundo escuro e translúcido
+  border-radius: 20px;
+  box-shadow: 0 4px 20px rgba(0, 255, 255, 0.5); // Sombra neon
+  padding: 20px;
+  width: 90%;
+  max-width: 800px;
+  border: 2px solid rgba(0, 255, 255, 0.5); // Borda neon
+  transition: box-shadow 0.3s ease;
 
   &:hover {
-    transform: scale(1.3); // Aumenta o tamanho do card ao passar o mouse
+    box-shadow: 0 8px 30px rgba(0, 255, 255, 1); // Sombra mais intensa ao passar o mouse
   }
 `;
 
+// Título
+const Title = styled.h1`
+  text-align: center;
+  color: #00ffff; // Cor neon
 
+`;
+
+// Contêiner de pesquisa
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+// Campo de entrada de pesquisa
+const SearchInput = styled.input`
+  width: 80%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid rgba(255, 0, 0, 0.5); // Borda mais fina e translúcida
+  background: rgba(30, 30, 30, 0.8); // Fundo escuro e translúcido
+  color: #00ffff; // Cor do texto neon
+  transition: border 0.2s, box-shadow 0.2s;
+
+  &:focus {
+    border-color: #00ffff; // Cor neon ao focar
+    outline: none;
+    box-shadow: 0 0 5px #00ffff; // Efeito de néon ao focar
+  }
+`;
+
+// Lista de Pokémon
+const PokemonList = styled.ul`
+  display: flex;
+  flex-wrap: wrap; // Permite que os cards se organizem em uma grade
+  list-style-type: none;
+  padding: 0;
+  justify-content: center;
+`;
+
+// Card de Pokémon
+const PokemonCard = styled.li`
+  border: 2px solid rgba(0, 255, 255, 0.3);
+  border-radius: 15px;
+  margin: 10px;
+  padding: 10px;
+  background: rgba(30, 30, 30, 0.8); // Fundo escuro e translúcido
+  transition: transform 0.2s, box-shadow 0.2s;
+  flex: 1 1 200px; // Ajuste para criar uma grade mais uniforme
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 10px rgba(0, 255, 255, 0.5); // Sombra ao passar o mouse
+  }
+`;
+
+// Conteúdo do card de Pokémon
 const CardContent = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
+// Imagem do Pokémon
 const PokemonImage = styled.img`
-  width: 100px;
-  height: 100px;
-  margin-right: 20px; /* Espaçamento entre a imagem e as informações */
+  width: 80px;
+  height: 80px;
+  margin-right: 20px;
 `;
 
+// Informações do Pokémon
 const PokemonInfo = styled.div`
-  text-align: left; /* Alinha o texto à esquerda */
+  text-align: left;
+  color: #ffffff; // Cor do texto
 `;
 
+// Tipos de Pokémon
 const Type = styled.span`
   display: inline-block;
   padding: 5px;
@@ -125,6 +186,7 @@ const Type = styled.span`
   color: white;
 
   &.grass {
+   
     background-color: #78C850;
   }
 
@@ -135,41 +197,49 @@ const Type = styled.span`
   &.water {
     background-color: #6890F0;
   }
+
   &.poison {
-    background-color:  #4a00f8;
+    background-color: #4a00f8;
   }
 
   &.bug {
-    background-color:  #fad327;
+    background-color: #fad327;
   }
-  
+
   &.ghost {
     background-color: #cc00ff;
   }
+
   &.flying {
     background-color: #009da8;
   }
+
   &.ground {
-    background-color:  #8d5000;
+    background-color: #8d5000;
   }
+
   &.rock {
     background-color: #0000009f;
   }
-  
+
   &.normal {
     background-color: #0000009f;
   }
+
   &.ice {
-    background-color:  #00f7ff;
+    background-color: #00f7ff;
   }
+
   &.psychic {
-    background-color:  #001aff;
+    background-color: #001aff;
   }
+
   &.electric {
-    background-color:  #fffc47;
+    background-color: #fffc47;
   }
+
   &.fighting {
-    background-color:  orange;
+    background-color: orange;
   }
 
   /* Adicione mais tipos conforme necessário */
